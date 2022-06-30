a8af50a6b54f1dbe35087cb3d393447ab7f8b474
John Christopher Azcarraga <dyankristoper@anonymous-mini.local>
2022-06-30 12:28:21 +0800

    Feat: Add initial items to state

--- src/App.js
+++ src/App.js
@@ -1,6 +1,20 @@
+import { useState } from 'react';
+
+import TodoItem from './components/TodoItem';
+
 const App = () => {
+  const initialItems = [
+    { id: 1, name: "Wake up", status: "pending" }
+  ];
+
+  const [ items, setItems ] = useState( initialItems );
+
   return (
-    <div>App</div>
+    <div>
+      {
+        items.map( item => <TodoItem name={item.name} key={item.id} /> )
+      }
+    </div>
   )
 }
 
